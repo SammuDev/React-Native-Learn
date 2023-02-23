bd82d0f65e38a7b7216454ea9e28a8c518e6a83d
SammuDev <samucas878@gmail.com>
2023-02-23 11:37:49 -0300

    init: First commit!

---
+++ src/components/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Title = () => {
+  return (
+    <View>
+      <Text>
+        Health
+      </Text>
+    </View>
+  );
+};
+
+export default Title;
