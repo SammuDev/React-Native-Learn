a11a14990c2909e0a312e0c4e0f1de2c0b1f3d24
SammuDev <samucas878@gmail.com>
2023-02-23 14:34:01 -0300

    style: Formatando a mensagem e o valor de IMC

--- src/components/Form/ResultIMC/index.jsx
+++ src/components/Form/ResultIMC/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import style from './style';
 
 const ResultIMC = (props) => {
   return (
-    <View>
-      <Text>{props.messageResultIMC} {props.resultIMC}</Text>
+    <View style={style.resultIMC}>
+      <Text style={style.information}>{props.messageResultIMC}</Text>
+      <Text style={style.numberIMC}>{props.resultIMC}</Text>
     </View>
   );
 };
--- src/components/Form/ResultIMC/style.jsx
+++ src/components/Form/ResultIMC/style.jsx
@@ -1,1 +1,24 @@
+import { StyleSheet } from 'react-native';
 
+const style = StyleSheet.create({
+  resultIMC: {
+    flex:1,
+    width: '100%',
+    marginTop: 15,
+    paddingTop: 60,
+    borderRadius: 50,
+    alignItems: 'center'
+  },
+  information: {
+    color: '#ff0043',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  numberIMC: {
+    color: '#ff0043',
+    fontSize: 48,
+    fontWeight: 'bold'
+  }
+});
+
+export default style;
